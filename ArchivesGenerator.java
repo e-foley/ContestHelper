4f88e027f32ac1c1efd70534fed9d34b646555ac
e-foley <efoley@calpoly.edu>
2017-01-01 16:40:37 -0800

    signature for archives generator
    I may make this an abstract class later.  Depends on my mood.

---
+++ ArchivesGenerator.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class ArchivesGenerator {
+    public ArchivesGenerator() {
+
+    }
+
+    public void generate(History history, BufferedWriter out, int contestStart, int contestEnd) {
+
+    }
+}
