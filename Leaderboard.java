92605a7bfbf86380d2b933f75df8d6a9a9923c4e
e-foley <efoley@calpoly.edu>
2017-02-21 22:59:18 -0800

    attempt to implement "get members at place"

--- Leaderboard.java
+++ Leaderboard.java
@@ -10,14 +10,47 @@ public class Leaderboard
 {
     private ArrayList<Member> members;
     private MemberDataRetriever metric;
+    private boolean is_sorted;
     
+    // TODO: Clone the members_set field so that additions to the list made outside this class don't mess up our assumptions about sorting
     public Leaderboard(ArrayList<Member> members_set, MemberDataRetriever metric_set) {
         members = members_set;
         metric = metric_set;
+        is_sorted = false;
     }
     
-    public void sort() {
+    private void sort() {
         Collections.sort(members, metric);
+        is_sorted = true;
+    }
+    
+    private void ensureOrder() {
+        if (!is_sorted) {
+            sort();
+        }
+    }
+    
+    // Problem: if you ask for a place that is smothered by a tie at a different place, not everybody at the same rank will be represented
+    public ArrayList<Member> getMembersAtPlace(int place) {
+        Member initial = members.get(place);
+        if (initial == null) {
+            return new ArrayList<Member>();
+        }
+        
+        ArrayList<Member> returning = new ArrayList<Member>();
+        returning.add(initial);
+        
+        for (int i = place + 1; i < members.size(); ++i) {
+            if (metric.compare(members.get(i), initial) == 0) {
+                // The two members are tied in this metric, so add the new member to our list
+                returning.add(members.get(i));
+            } else {
+                // The two members are not tied in this metric, so stop adding to our list of tied members
+                break;
+            }
+        }
+        
+        return returning;
     }
     
     // NOTE: `prefix` is not presently used
