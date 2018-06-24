fedec8f0a0bbffce1557c603fa63e500b43b1c29
e-foley <efoley@calpoly.edu>
2018-06-23 22:53:14 -0700

    html parser demo code
    (requires jsoup under lib)

---
+++ TestParse.java
@@ -0,0 +1,39 @@
+import org.jsoup.*;
+import org.jsoup.helper.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+
+/**
+ * Abstract class TestParse - write a description of the class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class TestParse
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y    a sample parameter for a method
+     * @return        the sum of x and y 
+     */
+    public static int doTheThing()
+    {
+        try {
+            Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+            System.out.println(doc.title());
+            Elements newsHeadlines = doc.select("#mp-itn b a");
+            for (Element headline : newsHeadlines) {
+              System.out.println(headline.attr("title"));
+              System.out.println(headline.absUrl("href"));
+            }
+        } catch (java.io.IOException e) {
+            return 1;
+        }
+        
+        return 42;
+    }
+}
