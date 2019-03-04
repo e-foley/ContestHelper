55e5d2764a12460f0d52131b7c6a7647f60cf098
e-foley <efoley@calpoly.edu>
2019-03-03 21:06:52 -0800

    elo compiles, at least

--- EloEvaluator.java
+++ EloEvaluator.java
@@ -10,18 +10,25 @@ import java.util.ArrayList;
 public class EloEvaluator
 {
     class RatingCalc {       
-        int rating_before = -1;
-        int rating_after = -1;
+        double rating_before = 0.0f;
+        double rating_after = 0.0f;
+        double stake = 0.0f;
     }
     
     // TODO: Rethink the inner container. There may be a better choice.
     private TreeMap<Integer, TreeMap<Integer, RatingCalc>> rating_history;
-    int starting_rating;
+    double starting_rating;
+    double base;
+    double divisor;
+    double aggressiveness;
 
-    public EloEvaluator()
+    public EloEvaluator(double starting_rating, double base, double divisor, double aggressiveness)
     {
         rating_history = new TreeMap<Integer, TreeMap<Integer, RatingCalc>>();
-        starting_rating = 1500;
+        this.starting_rating = starting_rating;
+        this.base = base;
+        this.divisor = divisor;
+        this.aggressiveness = aggressiveness;
     }
 
     public void evaluate(History history) {
@@ -60,19 +67,45 @@ public class EloEvaluator
                         this_calc.rating_before = last_calc.rating_after;
                     }
                     
+                    // TODO: There's already a notion of "stake" in Member.EntryStakePair... Maybe these could be linked somehow.
+                    this_calc.rating_after = this_calc.rating_before;  // (temp value)
+                    this_calc.stake = 1.0 / couples.size();
+                    
                     rating_history.get(new Integer(member_id)).put(poll.getSynch(), this_calc);
                 }
             }
                 
             // By this point, we have a provisional RatingCalc for this contest in every member's row.
-            // This RatingCalc has the correct "rating_before" field.
+            // This RatingCalc has the correct "rating_before" field and a temporary "rating_after" value to match.
             
             // Next, we want to calculate an expected result for every head-to-head matchup in this poll.
-            for (int j = 0; j < entries.size(); ++j) {
-                
-            }
-            
-                
+            for (int j = 0; j < entries.size() - 1; ++j) {                
+                for (int k = j + 1; k < entries.size(); ++k) {
+                    if (entries.get(j).numMembers() != 1 || entries.get(k).numMembers() != 1) {
+                        // Skip entries with multiple members contributing.
+                        // TODO: Change this.
+                        continue;
+                    }
+                    
+                    if (entries.get(j).getVotes() == 0 && entries.get(k).getVotes() == 0) {
+                        // Skip matchups with no information.
+                        continue;
+                    }
+                    
+                    // TODO: Our method treats the pool of votes available to be those that the two members collect together...
+                    // But it would be sweet if this took into account all other opponents and all other votes at the same time somehow.
+                    RatingCalc j_details = getRatingDetails(entries.get(j).getMemberNameCouples().get(0).member.getId(), poll.getSynch());
+                    RatingCalc k_details = getRatingDetails(entries.get(k).getMemberNameCouples().get(0).member.getId(), poll.getSynch());
+                    double q_j = Math.pow(base, (k_details.rating_before - j_details.rating_before) / divisor);
+                    double q_k = 1.0 - q_j;
+                    double s_j = entries.get(j).getVotes();
+                    double s_k = entries.get(k).getVotes();
+                    double e_j = q_j * (s_j + s_k);
+                    double e_k = q_k * (s_j + s_k);
+                    j_details.rating_after += aggressiveness * (s_j - e_j);
+                    k_details.rating_after += aggressiveness * (s_k - e_k);
+                }
+            }  
         }
         
     }
