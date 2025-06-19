17553aee6a5688b036844f18a808fa5937c8dc0a
Ed Foley <ed.l.foley@gmail.com>
2025-06-19 13:23:49 -0700

    backup -> input_cache, create dir if needed

--- src/Main.java
+++ src/Main.java
@@ -90,9 +90,11 @@ public abstract class Main
         try
         {
             stamps.add(new NamedStamp("Copying input files"));
-            // make backups of input
-            Main.copyFile(new File(input_origin + "input/data.txt"), new File(input_origin + "backup/data-" + history.getLastPollShortName() + ".txt"));
-            Main.copyFile(new File(input_origin + "input/associations.txt"), new File(input_origin + "backup/associations-" + history.getLastPollShortName() + ".txt"));
+            // Create cache directory if it doesn't exist.
+            new File(input_origin + "/input_cache").mkdirs();
+            // Cache input files. (The resulting files can serve as backups if something goes wrong.)
+            Main.copyFile(new File(input_origin + "input/data.txt"), new File(input_origin + "input_cache/data-" + history.getLastPollShortName() + ".txt"));
+            Main.copyFile(new File(input_origin + "input/associations.txt"), new File(input_origin + "input_cache/associations-" + history.getLastPollShortName() + ".txt"));
             
             // ARCHIVES
             stamps.add(new NamedStamp("Preparing archive generation"));
